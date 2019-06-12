42e962307e3e673def53537a0037e5017cb33bbe
keropyan <cacal_40@mai.ru>
2019-06-12 14:21:27 +0300

    2 commit

---
+++ js/game.js
@@ -0,0 +1,19 @@
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+var topRightDecor = new Image();
+var topLeftDecor = new Image();
+var ricardo = new Image();
+
+topLeftDecor.src = "img/jaw1.png";
+topRightDecor.src = "img/topRightDecor.png";
+ricardo.src = "img/ricardo.jpg";
+
+function draw() {
+    ctx.drawImage(topLeftDecor, 0, 0);
+    ctx.drawImage(topRightDecor, 1009, 40);
+    ctx.drawImage(ricardo, 450, 40);
+    requestAnimationFrame(draw);
+}
+
+draw()
